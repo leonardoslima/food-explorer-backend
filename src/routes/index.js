3e805a3a75b7b84f3f0faab208ebe42f66eda8b7
Leonardo Silva <leonardosivalima93@live.com>
2023-12-08 15:29:34 -0300

    AppError

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const usersRouter = require("./users.routes");
+
+const router = Router();
+router.use("/users", usersRouter);
+router.get("/", (request, response) =>
+  response.send(`<h1>Bem vindo a API FoodExplorer</h1>`)
+);
+
+module.exports = router;
