b98818d11cc288bd00b491ddef32ceedd4745390
Leonardo Silva <leonardosivalima93@live.com>
2023-12-12 16:41:37 -0300

    criando tabela de pratos e ingredientes

---
+++ src/database/knex/migrations/20231212193734_createDishes.js
@@ -0,0 +1,16 @@
+exports.up = (knex) =>
+  knex.schema.createTable("dishes", (table) => {
+    table.increments("id").primary();
+
+    table.text("name").notNullable();
+    table.text("category").notNullable();
+    table.float("price");
+
+    table.text("description");
+    table.text("photo");
+
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("dishes");
---
+++ src/database/knex/migrations/20231212193828_createIngredients.js
@@ -0,0 +1,14 @@
+exports.up = (knex) =>
+  knex.schema.createTable("ingredients", (table) => {
+    table.increments("id").primary();
+
+    table.text("name").notNullable();
+    table
+      .integer("dish_id")
+      .references("id")
+      .inTable("dishes")
+      .onDelete("CASCADE")
+      .notNullable();
+  });
+
+exports.down = (knex) => knex.schema.dropTable("ingredients");
