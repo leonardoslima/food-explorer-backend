92b3ef9f40edc2134ca9414f4468dab85fb64bea
Leonardo Silva <leonardosilvalima93@live.com>
2023-12-13 19:15:01 -0300

    index solicitações

--- src/controllers/RequestsController.js
+++ src/controllers/RequestsController.js
@@ -15,9 +15,40 @@ class RequestsController {
       throw new AppError("Prato não encontrado");
     }
 
-    await knex("requests").insert({ user_id, dish_id, quantity });
-    2;
+    const existingRequest = await knex("requests")
+      .where({ dish_id, user_id })
+      .first();
+
+    if (existingRequest) {
+      const newQuantity = Number(quantity) + existingRequest.quantity;
+      const updatedRequest = { ...existingRequest, quantity: newQuantity };
+
+      await knex("requests").update(updatedRequest).where({ user_id, dish_id });
+    } else {
+      await knex("requests").insert({ user_id, dish_id, quantity });
+    }
+
     return response.json();
+  }
+  async index(request, response) {
+    const { user_id } = request.params;
+
+    const userRequests = await knex("requests")
+      .select([
+        "requests.id",
+        "requests.quantity",
+        "dishes.name",
+        "dishes.price",
+        "dishes.photo",
+      ])
+      .innerJoin("dishes", "dishes.id", "requests.dish_id")
+      .where({ user_id });
+
+    const requestsWithSubTotal = userRequests.map((request) => {
+      return { ...request, subTotal: request.price * request.quantity };
+    });
+
+    return response.json(requestsWithSubTotal);
   }
 }
 
--- src/routes/requests.routes.js
+++ src/routes/requests.routes.js
@@ -6,5 +6,6 @@ const requestsController = new RequestsController();
 const requestsRoutes = Router();
 
 requestsRoutes.post("/", requestsController.create);
+requestsRoutes.get("/:user_id", requestsController.index);
 
 module.exports = requestsRoutes;
