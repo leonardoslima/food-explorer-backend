3529473741b6a426b19a78a4254fc346ed0444a8
Leonardo Silva <leonardosivalima93@live.com>
2023-12-14 15:18:04 -0300

    token usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const dishesRouter = require("./dishes.routes");
 const favoritesRouter = require("./favorites.routes");
 const requestsRouter = require("./requests.routes");
 const purchasesRouter = require("./purchases.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const router = Router();
 router.use("/users", usersRouter);
@@ -12,6 +13,7 @@ router.use("/dishes", dishesRouter);
 router.use("/favorites", favoritesRouter);
 router.use("/requests", requestsRouter);
 router.use("/purchases", purchasesRouter);
+router.use("/sessions", sessionsRouter);
 
 router.get("/", (request, response) =>
   response.send(`<h1>Bem vindo a API FoodExplorer</h1>`)
